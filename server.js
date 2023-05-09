e4cfe43c6f946445c3cfc56de698e7a0c680c337
Em <wiem.saidani@etu.univ-amu.fr>
2023-05-09 18:02:40 +0200

    add signInPage get/post in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mustache = require('mustache-express');
+const cookieSession = require('cookie-session');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -10,8 +11,24 @@ app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
 
+
+app.use(cookieSession({
+    secret: 'mot-de-passe-du-cookie'
+  }));
+// ----------------------------------------------------------------------------------
 app.get('/', function (req, res) {
     res.render('index');
   })
+
+  app.get('/signInPage', function (req, res) {
+    res.render('signInPage');
+  })
+
+  app.post('/signInPage', function (req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
+
+  })
+
 
 app.listen(5000, () => console.log('listening on http://localhost:5000'));
