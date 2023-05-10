5b761d7088347104df630f0af123f9d8c7faa4d8
Em <wiem.saidani@etu.univ-amu.fr>
2023-05-10 13:33:13 +0200

    modified server.js add signin and signout get/post

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ const cookieSession = require('cookie-session');
 
 const app = express();
 const bodyParser = require('body-parser');
+let flash = require('express-flash');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.static('public'));
 
 app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
-
+app.use(flash());
 
 app.use(cookieSession({
     secret: 'mot-de-passe-du-cookie'
   }));
+
+let userModel = require('./models/userModel');
+
 // ----------------------------------------------------------------------------------
-app.get('/', function (req, res) {
+  app.get('/', function (req, res) {
     res.render('index');
   })
 
@@ -24,10 +29,24 @@ app.get('/', function (req, res) {
     res.render('signInPage');
   })
 
+  app.get('/signOut', function (req, res) {
+    req.session = null;
+    res.redirect('/');
+  })
+
+
   app.post('/signInPage', function (req, res) {
     let email = req.body.email;
     let password = req.body.password;
-
+    let verifyId = userModel.loginBuddy(email,password);
+    if (verifyId == -1) {
+      req.flash('logError', 'email or password incorrect');
+      res.redirect('/signInPage');
+    }
+    else {
+        req.session.sessionId = verifyId;
+        res.redirect('/');
+    }
   })
 
 
