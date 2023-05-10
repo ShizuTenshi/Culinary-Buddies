56195cfd9b840c401d7b921a5525e6f7523b0749
Em <wiem.saidani@etu.univ-amu.fr>
2023-05-10 13:32:31 +0200

    modified createDB.js

--- createDB.js
+++ createDB.js
@@ -2,12 +2,11 @@ const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
 
-var load = function() {
+var createTables = function() {
 
-    db.prepare('DROP TABLE IF EXISTS test').run();
-    db.prepare('CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, password TEXT)').run();
-    db.prepare('INSERT INTO test (name, password) VALUES (@name, @password)').run(  {name: "Emory", password: "password"});
-    var test = db.prepare('SELECT * FROM test').run();
-    console.log(test);
+    db.prepare('DROP TABLE IF EXISTS Users').run();
+    db.prepare('CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)').run();
+    db.prepare('INSERT INTO USers (email, password) VALUES (@email, @password)').run(  {email: "Emory@culinaryBuddies", password: "password"});
 }
-load();
+
+createTables(); 
