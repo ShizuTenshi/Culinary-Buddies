adf026850543631e5a33bcce1b93d58bb60b9046
Em <wiem.saidani@etu.univ-amu.fr>
2023-05-11 13:45:39 +0200

    Edit createDB.js

--- createDB.js
+++ createDB.js
@@ -5,8 +5,8 @@ let db = new Sqlite('db.sqlite');
 var createTables = function() {
 
     db.prepare('DROP TABLE IF EXISTS Users').run();
-    db.prepare('CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)').run();
-    db.prepare('INSERT INTO USers (email, password) VALUES (@email, @password)').run(  {email: "Emory@culinaryBuddies", password: "password"});
+    db.prepare('CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)').run();
+    db.prepare('INSERT INTO USers (name, email, password) VALUES (@name, @email, @password)').run(  {name: "Emory" , email: "Emory@culinaryBuddies", password: "password"});
 }
 
 createTables(); 
