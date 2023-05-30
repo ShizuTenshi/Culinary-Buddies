2848437b8c7e40c002aff6c38f60d9fcd6426f83
Em <wiem.saidani@etu.univ-amu.fr>
2023-05-30 12:57:03 +0200

    Edit userModel functions + add updateUser

--- models/userModel.js
+++ models/userModel.js
@@ -5,24 +5,48 @@ const Sqlite = require('better-sqlite3');
 let db = new Sqlite('db.sqlite');
 
 exports.loginBuddy = (email,password) => {
-    let id = db.prepare("SELECT id FROM Users WHERE email = ? AND password = ? ").get([email,password]);
+    let id = db.prepare("SELECT accountId FROM Account WHERE email = ? AND password = ? ").get([email,password]);
     if (id == null) return -1;
     return id;
 }
 
-exports.nameExist = (name) => {
-    let id = db.prepare("SELECT id FROM Users WHERE name = ? ").get([name]);
+
+exports.nameExist = (username) => {
+    let id = db.prepare("SELECT accountId FROM Account WHERE username = ? ").get([username]);
     if (id == null) return -1;
     return id;
 }
 
 exports.emailExist = (email) => {
-    let id = db.prepare("SELECT id FROM Users WHERE email = ? ").get([email]);
+    let id = db.prepare("SELECT accountId FROM Account WHERE email = ? ").get([email]);
     if (id == null) return -1;
     return id;
 }
 
-exports.insertUser = (name,email,password) => {
-    let user = db.prepare('INSERT INTO Users (name, email, password) VALUES (?, ?, ?)').run([name,email,password]).lastInsertRowid;
+exports.insertUser = (username,email,password) => {
+    let user = db.prepare('INSERT INTO Account (username, email, password) VALUES (?, ?, ?)').run([username,email,password]).lastInsertRowid;
 }
 
+  // Update a user's account information based on their ID
+  exports.updateUser = (accountId, updatedData) => {
+    const query = `
+      UPDATE Account SET
+        email = ?,
+        username = ?,
+        password = ?,
+        authPost = ?,
+        healthConcernId = ?,
+        dietaryPreferenceId = ?
+      WHERE accountId = ?
+    `;
+    db.prepare(query).run(
+      updatedData.email,
+      updatedData.username,
+      updatedData.password,
+      updatedData.authPost,
+      updatedData.healthConcernId,
+      updatedData.dietaryPreferenceId,
+      accountId
+    );
+  };
+  
