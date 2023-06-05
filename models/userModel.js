3d2bf7d0f23df71ef1bfd005ea9ded5faac2603e
Em <wiem.saidani@etu.univ-amu.fr>
2023-06-05 14:15:45 +0200

    edit userModel = add updateAccount + getUsername

--- models/userModel.js
+++ models/userModel.js
@@ -4,49 +4,40 @@ const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
 
-exports.loginBuddy = (email,password) => {
-    let id = db.prepare("SELECT accountId FROM Account WHERE email = ? AND password = ? ").get([email,password]);
-    if (id == null) return -1;
-    return id;
+exports.loginBuddy = (email, password) => {
+  let id = db.prepare("SELECT accountId FROM Account WHERE email = ? AND password = ? ").get([email, password]);
+  if (id == null) return -1;
+  return id;
 }
 
 
 exports.nameExist = (username) => {
-    let id = db.prepare("SELECT accountId FROM Account WHERE username = ? ").get([username]);
-    if (id == null) return -1;
-    return id;
+  let id = db.prepare("SELECT accountId FROM Account WHERE username = ? ").get([username]);
+  if (id == null) return -1;
+  return id;
 }
 
 exports.emailExist = (email) => {
-    let id = db.prepare("SELECT accountId FROM Account WHERE email = ? ").get([email]);
-    if (id == null) return -1;
-    return id;
+  let id = db.prepare("SELECT accountId FROM Account WHERE email = ? ").get([email]);
+  if (id == null) return -1;
+  return id;
 }
 
-exports.insertUser = (username,email,password) => {
-    let user = db.prepare('INSERT INTO Account (username, email, password) VALUES (?, ?, ?)').run([username,email,password]).lastInsertRowid;
+exports.insertUser = (username, email, password) => {
+  let accountId = db.prepare('INSERT INTO Account (username, email, password) VALUES (?, ?, ?)').run([username, email, password]).lastInsertRowid;
+  return accountId;
 }
 
-  // Update a user's account information based on their ID
-  exports.updateUser = (accountId, updatedData) => {
-    const query = `
-      UPDATE Account SET
-        email = ?,
-        username = ?,
-        password = ?,
-        authPost = ?,
-        healthConcernId = ?,
-        dietaryPreferenceId = ?
-      WHERE accountId = ?
-    `;
-    db.prepare(query).run(
-      updatedData.email,
-      updatedData.username,
-      updatedData.password,
-      updatedData.authPost,
-      updatedData.healthConcernId,
-      updatedData.dietaryPreferenceId,
-      accountId
-    );
-  };
-  
+
+exports.updateAccount = (user, email, username, password) => {
+  db.prepare('UPDATE Account SET email = ?,  username = ?, password = ? WHERE accountId = ?').run(email, username, password, user);
+}
+
+
+exports.getUsernameFromId = (user) => {
+  console.log("dd ", user)
+  let username = db.prepare('SELECT username FROM Account WHERE accountId = ?').get([user]);
+  return username;
+}
+
+
