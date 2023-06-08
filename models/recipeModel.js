48aafc69bde115d7d2dde83ce6267ad3182c6e7d
Em <wiem.saidani@etu.univ-amu.fr>
2023-06-08 21:16:42 +0200

    edit recipeModel.js

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -3,8 +3,9 @@
 const Sqlite = require('better-sqlite3');
 let db = new Sqlite('db.sqlite');
 
-exports.addNewRecipe = (name, description, photo, preparationTime, cookTime, instructions) => {
-    let recipeId = db.prepare('INSERT INTO Recipe (name, description, photo, preparationTime, cookTime, instructions) VALUES (?, ?, ?,?,?, ?)').run([name, description, photo, preparationTime, cookTime, instructions]);
+exports.addNewRecipe = (name, description, photo, preparationTime, cookTime, instructions, accountId) => {
+    console.log("account id = ", accountId);
+    let recipeId = db.prepare('INSERT INTO Recipe (name, description, photo, preparationTime, cookTime, instructions, accountId) VALUES (?, ?, ?,?,?, ?, ?)').run([name, description, photo, preparationTime, cookTime, instructions, accountId]);
     return recipeId.lastInsertRowid;
 }
 
@@ -23,4 +24,36 @@ exports.addNewIngredients = (recipeId, combinedIngredients) => {
         console.log(recipeId, combinedIngredients[i].ingredient, combinedIngredients[i].quantity, combinedIngredients[i].unit);
         db.prepare('INSERT INTO Ingredient (recipeId, name, quantity, unit) VALUES (?, ?, ?, ?)').run([recipeId, combinedIngredients[i].ingredient, combinedIngredients[i].quantity, combinedIngredients[i].unit]);
     }
+}
+
+exports.getRecipeById = (recipeId) => {
+    let list = db.prepare("SELECT * FROM Recipe WHERE recipeId = ?").get([recipeId]);
+    return list;
+}
+
+exports.getTagByRecipeId = (recipeId) => {
+    let list = db.prepare("SELECT * FROM Tag WHERE recipeId = ?").all([recipeId]);
+    return list;
+}
+
+exports.getIngredientsByRecipeId = (recipeId) => {
+    let list = db.prepare("SELECT * FROM Ingredient WHERE recipeId = ?").all([recipeId]);
+    return list;
+}
+
+exports.getAllRecipesByAccountId = (accountId) => {
+    let list = db.prepare("SELECT * FROM Recipe WHERE accountId = ?").all([accountId]);
+    return list;
+}
+
+exports.getAllRecipes = () => {
+    let list = db.prepare("SELECT * FROM Recipe").all([]);
+    for (let x = 0; x < list.length; x++) {
+        let account = db.prepare("SELECT username FROM Account WHERE accountId=?").get([list[x].accountId]);
+        let category = db.prepare("SELECT categoryName FROM Category WHERE recipeId=?").get([list[x].recipeId]);
+        list[x].account = account;
+        list[x].category = category;
+
+    }
+    return list;
 }
